d1aaa9f7c015e59f59fe32d2f10bcc067d26715a
hiji <hijiapple3@gmail.com>
2019-05-12 01:23:12 +0000

    user pages

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $user->name }}</h3>
+                </div>
+                <div class="card-body">
+                    <!--<img class="rounded img-fluid" src= alt="">ここにアップロード画像が入る-->
+                </div>
+                <div>
+                <ul class="nav nav-tabs nav-justified">
+                    <li class="nav-item"><a href="#" class="nav-link">Fokllowings</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+                </ul>    
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <table class="table table-bordered">
+                <tr>
+                    <td>パート</td>
+                    <td>{{ $user->part }}</td>
+                </tr>
+            </table> 
+        </div>
+    </div>
+
+    <div class="row"> 
+        <div class="col-12">
+        <h2 class="text-center">MY POSTS</h2>
+            <div>
+                <!--{{ $user->posts }}-->
+            </div>
+        </div>
+    </div>   
+@endsection
